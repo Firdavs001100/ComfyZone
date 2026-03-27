0fcc7fd1679d99655609c00c4f2f251aa44fda99
Firdavs001100 <foruseofbusiness@gmail.com>
2026-03-28 08:22:11 +0900

    feat: prepare the project for production

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import server from "./app";
 
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+  override: true,
+});
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MONGODB connection success!");
-    const PORT = process.env.PORT ?? 3003;
+    const PORT = process.env.PORT ?? 3007;
     server.listen(PORT, () => {
       console.info(`The server is running successfully on port: ${PORT}`);
       console.info(`Admin project on http://localhost:${PORT}/admin \n`);

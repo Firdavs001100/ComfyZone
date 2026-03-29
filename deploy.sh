4db37ab49e769ab89f274d7c949e5b2373e3cdb8
Firdavs001100 <foruseofbusiness@gmail.com>
2026-03-29 14:30:43 +0900

    feat: add docker compose and finilize the deployment configurations

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 echo "🚀 Starting production deployment..."
 
 # Reset & pull latest code
-# git reset --hard
-# git checkout main
-# git pull origin main
+git reset --hard
+git checkout main
+git pull origin main
 
 npm i
 npm run build

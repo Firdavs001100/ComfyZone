2e4aa57d3c7a03456711bb7c80f110259ced43b3
Firdavs001100 <foruseofbusiness@gmail.com>
2026-04-02 07:36:18 +0900

    fix: try to resolve deployment issue with cors

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("./uploads"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://comfyzone.blog" }));
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
 
@@ -62,8 +62,7 @@ const server = http.createServer(app);
 
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "https://comfyzone.blog", // frontend domain
-    methods: ["GET", "POST"],
+    origin: true,
     credentials: true,
   },
 });
